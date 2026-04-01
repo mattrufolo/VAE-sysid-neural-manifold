c8e75416865eaae31f61f00e705d219a8941a6ac
mattrufolo <matteo.rufolo@outlook.it>
2026-04-01 16:20:46 +0200

    update dynamic example

--- dynamic_example/ae.py
+++ dynamic_example/ae.py
@@ -3,6 +3,24 @@ import jax.numpy as jnp
 from typing import Callable, List
 import flax.linen as nn
 
+class VAE_Encoder(nn.Module):
+    # output_size: int
+    mlp_layers: List[int]
+    rnn_size: int = 128
+
+
+    def setup(self):
+        self.rnn = nn.Bidirectional(
+            nn.RNN(nn.GRUCell(self.rnn_size)), nn.RNN(nn.GRUCell(self.rnn_size))
+        )
+        self.mlp_mean = MLP(self.mlp_layers)
+        self.mlp_logstd = MLP(self.mlp_layers)
+
+    def __call__(self, y, u):
+        yu = jnp.concat((y, u), axis=-1)
+        rnn_feat = self.rnn(yu).mean(axis=-2)
+        return self.mlp_mean(rnn_feat), self.mlp_logstd(rnn_feat)
+        # return enc_mean, enc_logstd
 
 class Encoder(nn.Module):
     mlp_layers: List[int]
@@ -12,18 +30,6 @@ class Encoder(nn.Module):
         self.rnn = nn.Bidirectional(
             nn.RNN(nn.GRUCell(self.rnn_size)), nn.RNN(nn.GRUCell(self.rnn_size))
         )
-
-        # To this:
-        # self.rnn = nn.Bidirectional(nn.RNN(nn.GRUCell(
-        #     features=self.rnn_size, 
-        #     dtype=jnp.float64, 
-        #     param_dtype=jnp.float64
-        # )),nn.RNN(nn.GRUCell(
-        #     features=self.rnn_size, 
-        #     dtype=jnp.float64, 
-        #     param_dtype=jnp.float64
-        # )))
-
         self.mlp = MLP(self.mlp_layers)
 
     def __call__(self, y, u):
@@ -44,6 +50,18 @@ class Projector(nn.Module):
     def __call__(self, z):
         out = self.net(z)
         return self.unflatten(out)
+
+class VAE_Projector(nn.Module):
+    outputs: int
+    unflatten: Callable
+
+    def setup(self):
+        self.net = nn.Dense(self.outputs, use_bias=False)
+
+
+    def __call__(self, z):
+        # out = self.net(z)
+        return self.unflatten(self.net(z * 0.1)) 
 
 
 class EncoderProjector(nn.Module):
